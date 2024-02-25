e0eb0bcad5892109b0f45b88283abbb657aa7156
Hassan Shah <hassanashah10@gmail.com>
2024-02-25 13:54:27 +0400

    fixing organisation of images, adding functionality to viewRulesWindow.py, maybe other things too I don't really remember

--- windows/homescreen_window.py
+++ windows/homescreen_window.py
@@ -16,7 +16,7 @@ Creating Top Section of Window
 TopFrame = Frame(master=homescreen, width=1280, height=72, xpos=0, ypos=0, fg_color='green')
 homescreen.frames['TopFrame'] = TopFrame
 
-UserProfileIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\UserProfileIcon.png'))
+UserProfileIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\images\\UserProfileIcon.png'))
 UserProfileIcon = ctk.CTkImage(light_image=UserProfileIconImage, dark_image=UserProfileIconImage)
 UserProfileIcon.configure(size=(40, 40))
 TopFrame.images['UserProfileIcon'] = UserProfileIcon
@@ -34,7 +34,7 @@ ProfileButton.configure(font=('Georgia', 8))
 TopFrame.buttons['ProfileButton'] = ProfileButton
 
 
-SettingsIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\SettingsIcon.png'))
+SettingsIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\images\\SettingsIcon.png'))
 SettingsIcon = ctk.CTkImage(light_image=SettingsIconImage, dark_image=SettingsIconImage)
 SettingsIcon.configure(size=(40, 40))
 TopFrame.images['SettingsIcon'] = SettingsIcon
@@ -61,7 +61,7 @@ Creating Main Section of Window
 MainFrame = Frame(master=homescreen, width=1280, height=576, xpos=0, ypos=73, fg_color='blue')
 homescreen.frames['MainFrame'] = MainFrame
 
-NewGameSettingsIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\NewGameSettingsIcon.png'))
+NewGameSettingsIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\images\\NewGameSettingsIcon.png'))
 NewGameSettingsImage = ctk.CTkImage(light_image=NewGameSettingsIconImage, dark_image=NewGameSettingsIconImage)
 NewGameSettingsImage.configure(size=(200, 200))
 MainFrame.images['NewGameSettingsIcon'] = NewGameSettingsImage
@@ -76,7 +76,7 @@ MainFrame.topLevelWindows['newGameSettings'] = None
 newGameSettingsButton = Button(master=MainFrame, button_text='', button_image=NewGameSettingsImage, width=200, height=200, xpos=300, ypos=200, command=NewGame)
 MainFrame.buttons['NewGameSettingsButton'] = newGameSettingsButton
 
-LoadGameIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\LoadGameIcon.png'))
+LoadGameIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\images\\LoadGameIcon.png'))
 LoadGameIcon = ctk.CTkImage(light_image=LoadGameIconImage, dark_image=LoadGameIconImage)
 LoadGameIcon.configure(size=(200, 200))
 MainFrame.images['LoadGameIcon'] = LoadGameIcon
@@ -91,7 +91,7 @@ MainFrame.topLevelWindows['LoadGame'] = None
 LoadGameButton = Button(master=MainFrame, button_text='', button_image=LoadGameIcon, width=200, height=200, xpos=550, ypos=200, command=LoadGame)
 MainFrame.buttons['LoadGameButton']=LoadGameButton
 
-ViewSavedGamesIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\ViewSavedGamesIcon.png'))
+ViewSavedGamesIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\images\\ViewSavedGamesIcon.png'))
 ViewSavedGamesIcon = ctk.CTkImage(light_image=ViewSavedGamesIconImage, dark_image=ViewSavedGamesIconImage)
 ViewSavedGamesIcon.configure(size=(200,200))
 MainFrame.images['ViewSavedGamesIcon']=ViewSavedGamesIcon
@@ -111,7 +111,7 @@ Creating Bottom Section of Window
 BottomFrame = Frame(master=homescreen, width=1280, height=72, xpos=0, ypos=648, fg_color='green')
 homescreen.frames['BottomFrame']=BottomFrame
 
-RulesIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\RulesIcon.png'))
+RulesIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\images\\RulesIcon.png'))
 RulesIcon = ctk.CTkImage(light_image=RulesIconImage, dark_image=RulesIconImage)
 RulesIcon.configure(size=(40, 40))
 BottomFrame.images['RulesIcon'] = RulesIcon
@@ -123,10 +123,10 @@ def viewRules():
 
 
 BottomFrame.topLevelWindows['viewRulesWindow'] = None
-ViewRulesButton = Button(master=BottomFrame, button_text='View Rules', button_image=RulesIcon, width=60, height=60, xpos=3, ypos=0, command=viewRules, compound='top')
+ViewRulesButton = Button(master=BottomFrame, button_text='View Rules', button_image=RulesIcon, width=60, height=60, xpos=3, ypos=1, command=viewRules, compound='top')
 BottomFrame.buttons['ViewRulesButton']=ViewRulesButton
 
-DictionaryIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\DictionaryIcon.png'))
+DictionaryIconImage = Image.open(os.path.join(os.path.dirname(__file__), 'assets\\images\\DictionaryIcon.png'))
 DictionaryIcon = ctk.CTkImage(light_image=DictionaryIconImage, dark_image=DictionaryIconImage)
 DictionaryIcon.configure(size=(40,40))
 BottomFrame.images['DictionaryIcon']=DictionaryIcon
@@ -138,7 +138,7 @@ def viewDictionary():
 
 
 BottomFrame.topLevelWindows['viewDictionaryWindow'] = None
-ViewDictionaryButton = Button(master=BottomFrame, button_text='View Dictionary', button_image=DictionaryIcon, width=60, height=60, xpos=84, ypos=0, command=viewDictionary, compound='top')
+ViewDictionaryButton = Button(master=BottomFrame, button_text='View Dictionary', button_image=DictionaryIcon, width=60, height=60, xpos=84, ypos=1, command=viewDictionary, compound='top')
 BottomFrame.buttons['ViewDictionaryButton'] = ViewDictionaryButton
 
 GameVersion = Label(master=BottomFrame, xpos=1150, ypos=36, text='Version 1.0', font_type='Helvetica', font_size=24, fg_color='blue', text_color='green')
--- windows/viewRulesWindow.py
+++ windows/viewRulesWindow.py
@@ -1,7 +1,10 @@
 from windows.windowsTemplate import *
 from windows import homescreen_window
+import os
+import webbrowser
 
-
+rules = {'NASPASite': 'https://scrabbleplayers.org/w/Code_of_Conduct', 'WESPAPDF': os.path.join(os.path.dirname(__file__), 'assets\\rules\\wesparulesv4.pdf'), 'FFScSite': 'https://www.ffsc.fr/classique.php?page=reglements', 'FISESite': 'https://fisescrabble.org/reglamentos/'}
+rulesOptions = None
 def destroyViewRulesWindow(window, master):
     window.destroy()
     homescreen_window.BottomFrame.topLevelWindows['viewRulesWindow'] = None
@@ -20,12 +23,22 @@ def lockToViewRulesWindow(master):
             button.configure(state='disabled')
 
 
+def openRules():
+    option = rulesOptions.get()
+    optionReversed = option[::-1]
+    option = option[:option.index(' ')] + optionReversed[:optionReversed.index(' ')][::-1]
+    webbrowser.open(f'{rules[option]}')
+
+
 def createViewRulesWindow(master):
-    viewRulesWindow = ctk.CTkToplevel(master=master, width=600, height=400)
+    viewRulesWindow = ctk.CTkToplevel(master=master, width=200, height=40)
     viewRulesWindow.title('View Rules')
     viewRulesWindow.protocol("WM_DELETE_WINDOW", lambda: destroyViewRulesWindow(viewRulesWindow, master))
-    viewRulesFrame = Frame(master=viewRulesWindow, width=600, height=400, xpos=0, ypos=0, fg_color='blue')
-    TitleLabel = Label(master=viewRulesFrame, text='View Rules', xpos=10, ypos=10, font_type='Futura', font_size=24)
+    viewRulesFrame = Frame(master=viewRulesWindow, width=200, height=40, xpos=0, ypos=0, fg_color='blue')
+    openRulesButton = Button(master=viewRulesFrame, button_text='Open', command=openRules, xpos=150, ypos=10, width=30, height=10)
+    global rulesOptions
+    rulesOptions = ctk.CTkComboBox(viewRulesFrame, values=['NASPA - Site', 'WESPA - PDF', 'FFSc - Site', 'FISE - Site'])
+    rulesOptions.place(x=5, y=5)
     viewRulesWindow.attributes("-topmost", True)
     lockToViewRulesWindow(master=master)
     return viewRulesWindow
